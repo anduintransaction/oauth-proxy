05512bde300d2387fcf9510b82cc04b7796ca6fc
keimoon <keimoonvie@gmail.com>
2016-11-10 10:49:21 +0700

    Add build file

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+RELEASE=0.1.0
+dist=dist
+bin=oauth-proxy
+
+function build {
+    GOOS=$1 GOARCH=$2 RUNMODE=production go build -o $bin
+    package=$bin-$RELEASE-$1-$2.tar.gz
+    tar cvzf $package $bin
+    mv $package $dist
+    rm $bin
+}
+
+goru dist
+mkdir -p $dist
+build darwin amd64
+build linux amd64
